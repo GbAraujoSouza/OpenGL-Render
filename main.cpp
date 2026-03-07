b1bfeeb53179e0bbf6ad8b8b967f9142f15d5922
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2026-03-06 22:17:55 -0300

    Initial window that changes color on number input

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+		glClearColor(0.949f, 0.835f, 0.812f, 1.0f);
+	}
+	else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+		glClearColor(0.6f, 0.82f, 0.858f, 1.0f);
+	}
+}
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -16,5 +32,27 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// resize window viewport
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
